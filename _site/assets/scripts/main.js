8e6743af1ab6ab1ca2cfbb47c25b35849b616fe9
Shen Cong <shen@whoknows.com>
2015-05-31 23:58:48 -0400

    added event page

--- _site/assets/scripts/main.js
+++ _site/assets/scripts/main.js
@@ -1,11 +1,62 @@
 
 $(document).ready(function(){  
-  for(var i=0 ; i<10 ; i++) {
-    $('<div class="item"><img src="/assets/img/groupmuse/'+i+'.jpg" style="height: 300px;width: auto;margin: auto;"><div class="carousel-caption"></div></div>').appendTo('.carousel-inner');
-    $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+    for(var i=0 ; i<10 ; i++) {
+        $('<div class="item"><img src="/assets/img/groupmuse/'+i+'.jpg" style="height: 300px;width: auto;margin: auto;"><div class="carousel-caption"></div></div>').appendTo('.carousel-inner');
+        $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
 
-  }
-  $('.item').first().addClass('active');
-  $('.carousel-indicators > li').first().addClass('active');
-  $('#carousel-example-generic').carousel();
+    }
+    $('.item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    $('#carousel-example-generic').carousel();
+
+
+
+
+    loadGallery(true, 'a.thumbnail');
+
+    function disableButtons(counter_max, counter_current){
+        $('#show-previous-image, #show-next-image').show();
+        if(counter_max == counter_current){
+            $('#show-next-image').hide();
+        } else if (counter_current == 1){
+            $('#show-previous-image').hide();
+        }
+    }
+
+    function loadGallery(setIDs, setClickAttr){
+        var current_image,
+            selector,
+            counter = 0;
+
+        $('#show-next-image, #show-previous-image').click(function(){
+            if($(this).attr('id') == 'show-previous-image'){
+                current_image--;
+            } else {
+                current_image++;
+            }
+
+            selector = $('[data-image-id="' + current_image + '"]');
+            updateGallery(selector);
+        });
+
+        function updateGallery(selector) {
+            var $sel = selector;
+            current_image = $sel.data('image-id');
+            $('#image-gallery-caption').text($sel.data('caption'));
+            $('#image-gallery-title').text($sel.data('title'));
+            $('#image-gallery-image').attr('src', $sel.data('image'));
+            disableButtons(counter, $sel.data('image-id'));
+        }
+
+        if(setIDs == true){
+            $('[data-image-id]').each(function(){
+                counter++;
+                $(this).attr('data-image-id',counter);
+            });
+        }
+        $(setClickAttr).on('click',function(){
+            updateGallery($(this));
+        });
+    }
+
 });
